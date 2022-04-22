cb45a9aa445268b7e5520141a8fbd6f4592e095b
Onur Yurdagelen <yurdagelenonur7@gmail.com>
2022-04-22 19:41:28 +0300

    React.UseCallback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useMemo, useState } from 'react';
+import { useMemo, useState,useCallback } from 'react';
 import Header from './components/Header';
 
 
@@ -7,9 +7,11 @@ function App() {
   const [number, setNumber] = useState(0)
   const [text, setText] = useState('')
 
-  const data = useMemo(() =>{
-    return calculateObject(number) // number degeri her degistiginde tekrardan re-render edilir.
-  },[number]) 
+ 
+  const increment = useCallback(()=>{
+    setNumber((prevState)=> prevState + 1); 
+    // number degiskeni kullanildiginde re-render islemi gerceklestirilir.Callback function ile yapildiginda re-render islemi yapilmaz.
+  },[])
 
   // const data = calculateObject();
   // {} === {} ==> false doner.Ramdeki referanslari farklidir.
@@ -17,23 +19,16 @@ function App() {
   //Tanimladigimiz component'in disinda tanimlar isek re-render islemo gerceklesmez.
   return (
     <div className="App">
-       <Header data={data}/> 
+       <Header increment={increment}/> 
+      <hr />
        {/* 5 ten kucuk olduunda Header render edilmez. */}
       <h1>{number}</h1>
-      <button onClick={()=> setNumber(number +1)}>Click</button>
+
       <br /><br />
-      <input type="text" onChange={({target}) => setText(target.value)} />
+      <input type="text"  />
     </div>
   );
 }
 
-function calculateObject(number){
-  console.log("Calculating...");
-  for(let i = 0;i<10000;i++)
-  {
-  }
-  console.log("Calculating Compoleted!")
-  return {name: "Onur",number}
-}
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Header = ({number,data}) => {
+const Header = ({number,increment}) => {
     console.log("Header Component Re-rendered!");
   return (
     <div>
         Header - {number}
 
         <br /><br />
-        <code>{JSON.stringify(data)}</code>
+        <button onClick={increment}>Click</button>
+      
     </div>
   )
 }
